de9eaf845e36d5cf2df75a465849bd4863bad48c
vanderleipinto <vandecopinto@gmail.com>
2023-08-29 22:10:24 -0300

    Curso de Ruby - Conceitos Básicos (Aula 2) mission 2

---
+++ mission2-2.rb
@@ -0,0 +1,17 @@
+# Curso de Ruby - Conceitos Básicos (Aula 2)
+# https://www.youtube.com/watch?v=JH_aEjoD4C0&list=PLdDT8if5attEOcQGPHLNIfnSFiJHhGDOZ&index=2&ab_channel=OneBitCode
+
+puts "Digite um número:"
+num_1 = gets.chomp.to_i
+puts "Digite outro número:" 
+num_2 = gets.chomp.to_i
+
+
+puts "A soma de  #{num_1} e #{num_2} é: #{num_1+num_2}"
+puts "A subtração de  #{num_1} e #{num_2} é: #{num_1-num_2}"
+
+if num_2 == 0
+  puts "Erro divisão por 0"
+else
+  puts "A divisão de  #{num_1} e #{num_2} é: #{num_1/num_2}"  
+end
