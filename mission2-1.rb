33eac22d9d9c065076cbbc09b589cc0867bb43f7
vanderleipinto <vandecopinto@gmail.com>
2023-08-29 22:03:59 -0300

    Curso de Ruby - Conceitos Básicos (Aula 2) mission 2-1

---
+++ mission2-1.rb
@@ -0,0 +1,9 @@
+# Curso de Ruby - Conceitos Básicos (Aula 2)
+# https://www.youtube.com/watch?v=JH_aEjoD4C0&list=PLdDT8if5attEOcQGPHLNIfnSFiJHhGDOZ&index=2&ab_channel=OneBitCode
+
+puts "Digite o nome:"
+name = gets.chomp
+puts "Digite a sua idade:" 
+age = gets.chomp
+
+puts "Olá #{name} você tem #{age} anos."
