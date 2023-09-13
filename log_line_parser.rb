e357262c0ee76e092b8f92343507051330a8970f
vanderleipinto <vandecopinto@gmail.com>
2023-09-13 18:12:06 -0300

    Exercício do site exercism.org, trabalhando com strings

---
+++ log_line_parser.rb
@@ -0,0 +1,29 @@
+# https://exercism.org/tracks/ruby/exercises/log-line-parser/edit
+
+
+class LogLineParser
+  def initialize(line)
+    @line = line
+  end
+
+  def message
+    start = @line.index(':')
+    final = @line.length
+    msg = @line.slice(start+1 , final).strip
+    msg.gsub!(/[\t\r\n]/,'')
+    return msg
+  end
+
+  def log_level
+    level = @line.split(':')
+    level[0][1...].chop.downcase
+  end
+
+  def reformat
+    level = @line.split(':')    
+    information = "(#{level[0][1...level[0].size-1].chomp.downcase})"
+    information.strip!
+    
+    return "#{self.message} #{information}"
+  end
+end
