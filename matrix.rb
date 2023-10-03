32ae3d5cbaf8bb0382cb109fc10f470100dcb8eb
Vanderlei Carvalho Rodrigues Pinto <vandecopinto@gmail.com>
2023-10-03 20:00:12 -0300

    Create matrix exercism.org
    Instructions
    Given a string representing a matrix of numbers, return the rows and columns of that matrix.
    
    So given a string with embedded newlines like:
    
    9 8 7
    5 3 2
    6 6 7
    representing this matrix:
    
        1  2  3
      |---------
    1 | 9  8  7
    2 | 5  3  2
    3 | 6  6  7
    your code should be able to spit out:
    
    A list of the rows, reading each row left-to-right while moving top-to-bottom across the rows,
    A list of the columns, reading each column top-to-bottom while moving from left-to-right.
    The rows for our example matrix:
    
    9, 8, 7
    5, 3, 2
    6, 6, 7
    And its columns:
    
    9, 5, 6
    8, 3, 6
    7, 2, 7

---
+++ matrix.rb
@@ -0,0 +1,23 @@
+#https://exercism.org/tracks/ruby/exercises/matrix/edit
+class Matrix
+  def initialize(matrix)
+    substring = matrix.split("\n") #divide strings separadas por quebra de linha
+    @matrix = substring.map do |substring|
+      numbers = substring.split.map(&:to_i)
+      numbers
+    end  
+    debug @matrix
+  end
+
+  def row(row)
+    @matrix[row-1].flatten
+  end
+
+  def column(col)
+    result = []
+    @matrix.map do |line|
+      result.push(line[col-1])
+    end
+  result    
+  end
+end
