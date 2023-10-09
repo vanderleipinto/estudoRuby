c8cc9bc06c78953dfdcc9471c9d478409db3b9b1
vanderleipinto <vandecopinto@gmail.com>
2023-10-09 08:20:22 -0300

    Mission 10 inserida

--- mission10.rb
+++ mission10.rb
@@ -48,8 +48,15 @@ File.open('json.json', 'w') do |line|
 end
 
 req = Net::HTTP::Post.new("/api/users")
-# para fazer chamadas https
-req.set_form_data({name:'Vanderlei', job:'Developer'})
+# para fazer chamadas https será usado o use_ssl:true
+# req.set_form_data({name:'Vanderlei', job:'Developer'})
+
+# Cabeçalho se necessário
+req['Content-Type'] = 'application/json'
+
+# Corpo da solicitação
+req.body = '{"name": "Vanderlei", "email": "vander@vander.com"}'
+
 
 response = Net::HTTP.start('reqres.in', use_ssl: true) do |http|
   http.request(req)
@@ -58,4 +65,31 @@ end
 puts '\n\n\n----------------------\n\n\n'
 puts response.code
 puts response.body
+
+# WEB SCRAPING
+
+# Através dele conseguimos extrair dados de sites, como por exemplo: reulstados
+# dos jogos de futebol de um portal de notícias, uma lista de posts de um blog...
+# Para fazermos isto, usaremos uma biblioteca chamada Nokogiri, que nos ajudará
+# a encontrar as informações dentro das estruturas HTML dos sites
+
+require 'nokogiri'
+# require 'net/http'
+require 'open-uri'
+
+url = 'https://www.uniprot.org/id-mapping/'
+https = Net::HTTP.new(url, 443)
+
+https.use_ssl = true
+
+response = https.get("/")
+
+doc = Nokogiri::HTML(response.body)
+
+h1 = doc.at('h1')
+
+puts '\n\n\n\n\n\n'
+puts '\n\n\n----------------------\n\n\n'
+puts h1.content
+
 
