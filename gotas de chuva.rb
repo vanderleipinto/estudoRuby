765c7f7222e13a5a31039a2f87c70d89d00555c7
Vanderlei Carvalho Rodrigues Pinto <vandecopinto@gmail.com>
2023-10-12 15:49:10 -0300

    Update gotas de chuva.rb
    Added alternative version

--- gotas de chuva.rb
+++ gotas de chuva.rb
@@ -9,3 +9,14 @@ class Raindrops
     ret    
   end
 end
+
+#alternative version
+#class Raindrops
+#  RAINDROPS = {3 => "Pling", 5 => "Plang", 7 => "Plong" }
+#  
+#  def self.convert(number) 
+#    conversion  = ""
+#    RAINDROPS.each { |prime, sound| conversion += sound if number % prime == 0  }
+#    conversion.empty? ? number.to_s : conversion
+#  end
+#end
