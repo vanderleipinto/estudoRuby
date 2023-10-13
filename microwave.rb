608a4854ac25e451a4efdcc2de032ef63cfef4a1
Vanderlei Carvalho Rodrigues Pinto <vandecopinto@gmail.com>
2023-10-13 16:31:29 -0300

    Update microwave.rb
    alternative verssion

--- microwave.rb
+++ microwave.rb
@@ -22,3 +22,16 @@ class Microwave
     return "#{min}:#{sec}"
   end
 end
+
+#alternative version
+# class Microwave
+#   def initialize(time)
+#     @time = time
+#   end
+#   def timer
+#     hours, seconds = @time.divmod(100)
+#     hours += seconds / 60
+#     seconds %= 60
+#     "%02d:%02d" % [hours, seconds]
+#   end
+# end
