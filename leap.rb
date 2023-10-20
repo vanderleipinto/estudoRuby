981bd24f9fca3d1d903210478c3f60ab69b82322
Vanderlei Carvalho Rodrigues Pinto <vandecopinto@gmail.com>
2023-10-20 15:28:39 -0300

    Update leap #alternative solution

--- leap.rb
+++ leap.rb
@@ -4,3 +4,10 @@ class Year
     (year%100==0 ? (year%100==0) && year%400==0 : year%4==0)    
   end
 end
+
+#alternative solution
+#class Year
+#   def self.leap? year
+#     year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
+#   end
+# end
