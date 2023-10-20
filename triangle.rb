be3eb3fc91942fe4e0384dae6b24d6a57c054341
Vanderlei Carvalho Rodrigues Pinto <vandecopinto@gmail.com>
2023-10-20 00:01:17 -0300

    Update triangle alternative version

--- triangle.rb
+++ triangle.rb
@@ -25,3 +25,24 @@ class Triangle
     @triangle.permutation(3).all?{|a,b,c| c <= a+b}
   end
 end
+
+#alternative version
+class Triangle
+#   attr_reader :sides
+#   def initialize(sides)
+#     @sides = sides
+#   end
+#   def equilateral?
+#     triangle? && sides.uniq.count == 1
+#   end
+#   def isosceles?
+#     triangle? && sides.uniq.count <= 2
+#   end
+#   def scalene?
+#     triangle? && sides.uniq.count == 3
+#   end
+#   def triangle?
+#     @sides.all?(&:positive?) &&
+#       @sides.permutation(3).all? { |(a, b, c)| a <= b + c }
+#   end
+# end
