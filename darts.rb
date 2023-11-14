60ee7b06dd7b9db1755a2b250b357317e92a855d
Vanderlei Carvalho Rodrigues Pinto <vandecopinto@gmail.com>
2023-11-14 07:30:45 -0300

    Create darts exercism.org
    Instructions
    Write a function that returns the earned points in a single toss of a Darts game.
    
    Darts is a game where players throw darts at a target.
    
    In our particular instance of the game, the target rewards 4 different amounts of points, depending on where the dart lands:
    
    If the dart lands outside the target, player earns no points (0 points).
    If the dart lands in the outer circle of the target, player earns 1 point.
    If the dart lands in the middle circle of the target, player earns 5 points.
    If the dart lands in the inner circle of the target, player earns 10 points.
    The outer circle has a radius of 10 units (this is equivalent to the total radius for the entire target), the middle circle a radius of 5 units, and the inner circle a radius of 1. Of course, they are all centered at the same point — that is, the circles are concentric defined by the coordinates (0, 0).
    
    Write a function that given a point in the target (defined by its Cartesian coordinates x and y, where x and y are real), returns the correct amount earned by a dart landing at that point.

---
+++ darts.rb
@@ -0,0 +1,15 @@
+#https://exercism.org/tracks/ruby/exercises/darts/edit
+class Darts
+  def initialize (x,y)
+    @x=x
+    @y=y    
+  end
+
+  def score
+    r = Math.sqrt(@x**2 + @y**2)
+    return 10 if r <= 1
+    return 5 if r <= 5
+    return 1 if r <= 10
+    return 0 if r>10
+  end
+end
