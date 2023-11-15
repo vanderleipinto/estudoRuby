e33312544704317e163b49b6df5958e145466ce4
Vanderlei Carvalho Rodrigues Pinto <vandecopinto@gmail.com>
2023-11-15 16:11:15 -0300

    Update phone_number alternative
    Added alternative version

--- phone_number.rb
+++ phone_number.rb
@@ -18,4 +18,15 @@ class PhoneNumber
     return arr.join('')    
       end
   end
+
+#alternative version
+
+module PhoneNumber
+  VALID_PHONE_NUMBER = /^([2-9]\d\d){2}\d{4}$/
+  def self.clean(number)
+    numbers = number.gsub(/\D/, '').sub(/^1/, '')
+    numbers[VALID_PHONE_NUMBER]
+  end
+end
+
       
