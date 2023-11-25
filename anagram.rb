91fb3b93d04b5271d0cccf40b4686b17ab17e384
Vanderlei Carvalho Rodrigues Pinto <vandecopinto@gmail.com>
2023-11-25 08:56:44 -0300

    Update anagram exercism.org
    Instructions
    An anagram is a rearrangement of letters to form a new word: for example "owns" is an anagram of "snow". A word is not its own anagram: for example, "stop" is not an anagram of "stop".
    
    Given a target word and a set of candidate words, this exercise requests the anagram set: the subset of the candidates that are anagrams of the target.
    
    The target and candidates are words of one or more ASCII alphabetic characters (A-Z and a-z). Lowercase and uppercase characters are equivalent: for example, "PoTS" is an anagram of "sTOp", but StoP is not an anagram of sTOp. The anagram set is the subset of the candidate set that are anagrams of the target (in any order). Words in the anagram set should have the same letter case as in the candidate set.
    
    Given the target "stone" and candidates "stone", "tones", "banana", "tons", "notes", "Seton", the anagram set is "tones", "notes", "Seton".

--- anagram.rb
+++ anagram.rb
@@ -1,13 +1,20 @@
 #https://exercism.org/tracks/ruby/exercises/anagram/edit
 class Anagram
   def initialize(word)
-    @word = word
+    @word = word.downcase.gsub(/\s+/, "")
   end
   def match(arr)
-    arr_word = @word.downcase.chars
-    ret = arr.select do |w|
-       w.chars.all?{ |i| arr_word.include?(i)}
-    end
-    ret.select{|i| i.length == @word.length}
-  end
+   word1_count = Hash.new(0)
+   word2_count = Hash.new(0)
+    ret = []
+
+    @word.each_char {|c| word1_count[c]+=1}
+
+    arr.each do |arr_word|
+      arr_word.downcase.gsub(/\s+/, "").each_char {|c| word2_count[c]+=1}
+      ret << arr_word if word1_count == word2_count
+      word2_count = Hash.new(0)
+    end  
+    ret.select{|w| w.downcase.gsub(/\s+/, "") != @word}
+  end   
 end
