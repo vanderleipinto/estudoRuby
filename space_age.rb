c51fd21be5985d60cb710efdb850d21bc9499880
Vanderlei Carvalho Rodrigues Pinto <vandecopinto@gmail.com>
2023-11-26 02:18:22 -0300

    Create space_age exercism.org
    Instructions
    Given an age in seconds, calculate how old someone would be on:
    
    Mercury: orbital period 0.2408467 Earth years
    Venus: orbital period 0.61519726 Earth years
    Earth: orbital period 1.0 Earth years, 365.25 Earth days, or 31557600 seconds
    Mars: orbital period 1.8808158 Earth years
    Jupiter: orbital period 11.862615 Earth years
    Saturn: orbital period 29.447498 Earth years
    Uranus: orbital period 84.016846 Earth years
    Neptune: orbital period 164.79132 Earth years
    So if you were told someone were 1,000,000,000 seconds old, you should be able to say that they're 31.69 Earth-years old.
    
    If you're wondering why Pluto didn't make the cut, go watch this YouTube video.
    
    Note: The actual length of one complete orbit of the Earth around the sun is closer to 365.256 days (1 sidereal year). The Gregorian calendar has, on average, 365.2425 days. While not entirely accurate, 365.25 is the value used in this exercise. See Year on Wikipedia for more ways to measure a year.

---
+++ space_age.rb
@@ -0,0 +1,46 @@
+class SpaceAge
+  def initialize(seconds)
+    @sec = seconds
+    @earth_year_seconds = 31557600
+    @orbital = {
+      mercury:  0.2408467,
+      venus: 0.61519726 ,
+      mars: 1.8808158,
+      jupiter:  11.862615,
+      saturn:  29.447498,
+      uranus: 84.016846,
+      neptune: 164.79132
+    }
+  end
+  def on_earth
+     @sec.fdiv(@earth_year_seconds).round(2)
+  end
+  def on_mercury
+     
+     orbital_calculate(@orbital[:mercury])
+  end
+  def on_venus     
+     orbital_calculate(@orbital[:venus])     
+  end
+  def on_mars     
+     orbital_calculate(@orbital[:mars])     
+  end
+  def on_jupiter     
+     orbital_calculate(@orbital[:jupiter])     
+  end
+  
+  def on_saturn
+     orbital_calculate(@orbital[:saturn])     
+  end
+  
+  def on_uranus   
+     orbital_calculate(@orbital[:uranus])     
+  end  
+  def on_neptune     
+     orbital_calculate(@orbital[:neptune])     
+  end  
+  
+  def orbital_calculate(orbital)
+    (@sec.fdiv(@earth_year_seconds)).fdiv(orbital).round(2)
+  end
+end
