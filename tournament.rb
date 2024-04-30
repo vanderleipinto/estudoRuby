6a3283d9b48be7fc8e002afb5f8a4e191b34d776
vanderleipinto <vandecopinto@gmail.com>
2024-04-30 13:29:33 -0300

    Exercism Tournament.rb

--- tournament.rb
+++ tournament.rb
@@ -1,65 +1,54 @@
-#https://exercism.org/tracks/ruby/exercises/tournament/edit
+# https://exercism.org/tracks/ruby/exercises/tournament/edit
 class Tournament
   @TALLY = "Team                           | MP |  W |  D |  L |  P\n"
-  @teams = {"Devastating Donkeys"=>{'MP'=>0,'W'=>0,'D'=>0,'L'=>0,'P'=>0},"Allegoric Alaskans"=>{'MP'=>0,'W'=>0,'D'=>0,'L'=>0,'P'=>0},"Blithering Badgers"=>{'MP'=>0,'W'=>0,'D'=>0,'L'=>0,'P'=>0},"Courageous Californians"=>{'MP'=>0,'W'=>0,'D'=>0,'L'=>0,'P'=>0}}
+  @teams = {
+    "Devastating Donkeys" => {"name" => "Devastating Donkeys            ", "MP" => 0, "W" => 0, "D" => 0, "L" => 0, "P" => 0},
+    "Allegoric Alaskans" => {"name" => "Allegoric Alaskans             ", "MP" => 0, "W" => 0, "D" => 0, "L" => 0, "P" => 0},
+    "Blithering Badgers" => {"name" => "Blithering Badgers             ", "MP" => 0, "W" => 0, "D" => 0, "L" => 0, "P" => 0},
+    "Courageous Californians" => {"name" => "Courageous Californians        ", "MP" => 0, "W" => 0, "D" => 0, "L" => 0, "P" => 0}
+  }.freeze
 
-  def self.tally (input)
-        
+  def self.tally(input)
     return @TALLY if input.length <= 1
+    teams = Marshal.load(Marshal.dump(@teams))
+    lines = input.split("\n").each { |line| line.strip! }
+    array = []
+    input.each_line { |line| array << line.strip!.split(";") }
+    ret = ""
 
-    lines = input.split("\n").each{|line| line.strip!}
-    ret = ''
-    
-    
-    lines.each do |line|    
-      str = line.split(';')        
-      if str[2] == 'win' 
-        @teams[str[0]]['MP']+=1
-        @teams[str[0]]['W']+=1
-        @teams[str[0]]['P']+=3
-        @teams[str[1]]['MP']+=1
-        @teams[str[1]]['L']+=1
+    array.each do |str|
+      if str[2] == "win"
+        teams[str[0]]["MP"] += 1
+        teams[str[0]]["W"] += 1
+        teams[str[0]]["P"] += 3
+        teams[str[1]]["MP"] += 1
+        teams[str[1]]["L"] += 1
       end
-      if str[2] == 'draw' 
-        @teams[str[0]]['MP']+=1
-        @teams[str[0]]['D']+=1
-        @teams[str[0]]['P']+=1
-        @teams[str[1]]['MP']+=1
-        @teams[str[1]]['D']+=1
-        @teams[str[1]]['P']+=1
+      if str[2] == "draw"
+        teams[str[0]]["MP"] += 1
+        teams[str[0]]["D"] += 1
+        teams[str[0]]["P"] += 1
+        teams[str[1]]["MP"] += 1
+        teams[str[1]]["D"] += 1
+        teams[str[1]]["P"] += 1
       end
-      if str[2] == 'loss' 
-        @teams[str[1]]['MP']+=1
-        @teams[str[1]]['W']+=1
-        @teams[str[1]]['P']+=3
-        @teams[str[0]]['MP']+=1
-        @teams[str[0]]['L']+=1
+      if str[2] == "loss"
+        teams[str[1]]["MP"] += 1
+        teams[str[1]]["W"] += 1
+        teams[str[1]]["P"] += 3
+        teams[str[0]]["MP"] += 1
+        teams[str[0]]["L"] += 1
       end
-  
+    end
+    p teams
+    mostrar_valores(teams)
   end
 
-  @teams = @teams.sort_by{|key,value| [-value['P'],value[name]]}.to_h
-    
-    @teams.each_pair do |key,value|
-        if value['MP'] != 0 ???????   
-          ret << key.ljust(31)
-          value.each_pair {|key2,value2| ret << "|  #{value2} "} 
-          ret += "\n"
-        end
-    end
-    return @TALLY + ret
+  def self.mostrar_valores(teams)
+    str = @TALLY
+    teams.sort_by { |_, item| [-item["P"], -item["name"], -item["W"]] }
+      .filter { |_, item| item["MP"] != 0 }
+      .each { |_, value| str += "#{value["name"]}|  #{value["MP"]} |  #{value["W"]} |  #{value["D"]} |  #{value["L"]} | #{value["P"].to_s.rjust(2)}\n" }
+    str
   end
 end
-
-a="Allegoric Alaskans;Blithering Badgers;win\nDevastating Donkeys;Courageous Californians;draw\n     Devastating Donkeys;Allegoric Alaskans;win\n     Courageous Californians;Blithering Badgers;loss\n     Blithering Badgers;Devastating Donkeys;loss\n     Allegoric Alaskans;Courageous Californians;win"
-
-puts Tournament.tally(a)
-  
-
-["Allegoric Alaskans;Blithering Badgers;win",
- "Devastating Donkeys;Courageous Californians;draw",
- "Devastating Donkeys;Allegoric Alaskans;win",
- "Courageous Californians;Blithering Badgers;loss",
- "Blithering Badgers;Devastating Donkeys;loss",
- "Allegoric Alaskans;Courageous Californians;win"]
-
