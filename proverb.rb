a45e6055c27858710c28e70ff1d63b23b7b01a4e
vanderleipinto <vandecopinto@gmail.com>
2024-05-02 17:52:40 -0300

    Exercism Proverb reduced

--- proverb.rb
+++ proverb.rb
@@ -12,3 +12,13 @@ class Proverb
     ret << "And all for the want of a %s." % @first
   end
 end
+
+# reduced
+# class Proverb
+#   attr_reader :to_s
+
+#   def initialize(*chain, qualifier: nil)
+#     @to_s = chain.each_cons(2).inject('') { |out, e| out << "For want of a #{e[0]} the #{e[1]} was lost.\n" } +
+#       "And all for the want of a #{qualifier ? "#{qualifier} " : ''}#{chain[0]}."
+#   end
+# end
